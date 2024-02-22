a5785a800283617026009a58d4a8e283250b2280
jlessberg <71854897+jlessberg@users.noreply.github.com>
2024-02-22 16:00:46 -0300

    Add files via upload

---
+++ js/testimonial-scripts.js
@@ -0,0 +1,53 @@
+//   all ------------------
+function initParadoxWay() {
+    "use strict";
+   
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+                
+            }
+        });
+    }
+    
+    // bubbles -----------------
+    
+    
+    setInterval(function () {
+        var randomValue = 2; //ANTES esta linea no existia//
+        var size = randomValue; //ANTES randomValue(sArray);//
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>'); //Antes randomValue(bArray)//
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity': '-=0.7'
+        }, 4000, function () {
+            $(this).remove()
+        });
+    }, 350);
+    
+    // 
+}
+
+//   Init All ------------------
+$(document).ready(function () {
+    initParadoxWay();
+});
